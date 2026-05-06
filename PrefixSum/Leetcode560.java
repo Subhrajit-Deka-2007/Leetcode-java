5bfb9c65fa78b443ad7fb82839c5b92e88081564
Subhrajit Deka <dekasubhrajit2007@gmail.com>
2026-05-06 08:04:05 +0530

    Commiting

---
+++ PrefixSum/Leetcode560.java
@@ -0,0 +1,37 @@
+package PrefixSum;
+
+
+public class Leetcode560 {
+    public static void main(String[] args) {
+    }
+        public int subarraySum(int[] arr , int k)
+        {
+            int count =0;
+            int sum = 0;
+
+            for( int i = 0 ; i < arr.length-1  ; i++ )
+            {
+
+
+                if(  arr[i] == k ) count++;
+
+                sum = arr[i];
+
+                for( int j = i+1 ; j<arr.length; j++  )
+                {
+
+                    if( sum + arr[j]  == k )count ++;
+                    sum +=arr[j];
+                }
+
+            }
+
+            if( arr[ arr.length-1 ] == k )count++;
+            return count ;
+        /*
+        T.C =O(N^2)
+        S.C =O(1)
+        */
+        }
+    }
+
